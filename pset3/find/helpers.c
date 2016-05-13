c2bd27e047533ded984c5d6fd1c940dc918f07fb
Shivam Waghela <sbw-psm@users.noreply.github.com>
2016-05-13 15:58:43 +0530

    Update helpers.c

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -37,7 +37,7 @@ void sort(int values[], int n)
     // implementation of bubble sort algorithm
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n-i; j++)
+        for (int j = 0; j < n-1-i; j++)
         {
            if (values[j] > values[j+1])
            {
